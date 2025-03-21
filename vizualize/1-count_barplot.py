9fabfa71f7cf18bd7a55d8decbc3399ae29beaf3
Nikolay Melnikov <nic11371@gmail.com>
2025-03-21 12:27:56 +0300

    wine.csv

--- vizualize/1-count_barplot.py
+++ vizualize/1-count_barplot.py
@@ -1,5 +1,11 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+
+train_data = pd.read_csv('vizualize/fixtures/Wine.csv', index_col=0)
+train_data = train_data.fillna(0)
+print(train_data.columns.to_list())
 
 
 # BEGIN (write your solution here)
@@ -18,3 +24,6 @@ def get_barplot(df, col_name, y_name, figsize=(7, 7)):
     plt.title(f'Mean {y_name} for {col_name}', fontsize=16)
     sns.barplot(x=col_name, y=y_name, data=df)
 # END
+
+
+get_countplot(train_data, 'country')
