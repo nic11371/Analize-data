fcafccfcdbcee3dc77b402ab8c406c14d65a8523
Nikolay Melnikov <nic11371@gmail.com>
2025-03-25 12:51:02 +0300

    seaborn

---
+++ vizualize/5-seaborn.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+# Готовим датафрейм
+df = pd.read_csv('taxi_orders.csv')
+df['datetime'] = pd.to_datetime(df['datetime'])
+df['hour'] = df['datetime'].dt.hour
+
+# BEGIN (write your solution here)
+def get_orders():
+    sns.barplot(
+        x='hour',
+        y='num_orders',
+        palette='hls',
+        data=df
+    )
+    plt.title('Количество заказов такси по часам')
+    plt.grid()
+    plt.xlabel('Часы')
+    plt.ylabel('Количество заказов')
+    plt.savefig('vizualize/results/seaborn.png')
+# END
